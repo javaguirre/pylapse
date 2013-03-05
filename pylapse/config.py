bd5778f16944062b9e262d60d3f79ae8fcad2b55
Javier Aguirre <contacto@javaguirre.net>
2013-03-05 17:23:38 +0100

    Expanding HOME path if path startwith ~

--- pylapse/config.py
+++ pylapse/config.py
@@ -10,7 +10,15 @@ if os.path.exists(CONFIG_PATH):
 
 DELAY = float(config.get('pylapse', 'DELAY'))
 CAPTURES_PATH = config.get('pylapse', 'CAPTURES_PATH')
+
+if CAPTURES_PATH.startswith('~'):
+    CAPTURES_PATH = CAPTURES_PATH.replace('~', HOME_PATH, 1)
+
 VIDEOS_PATH = config.get('pylapse', 'VIDEOS_PATH')
+
+if VIDEOS_PATH.startswith('~'):
+    VIDEOS_PATH = VIDEOS_PATH.replace('~', HOME_PATH, 1)
+
 IMAGE_EXT = config.get('pylapse', 'IMAGE_EXT')
 VIDEO_DEVICE = config.get('pylapse', 'VIDEO_DEVICE')
 RESOLUTION = config.get('pylapse', 'RESOLUTION')
